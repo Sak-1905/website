a9b3c8a3391c06a65b08e8dc93bacaae2fb3d52c
Nomekuma <t.gunness35@gmail.com>
2023-10-12 12:30:26 +0400

    deployment

--- final-website/website_ristana/website_ristana/settings.py
+++ final-website/website_ristana/website_ristana/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-e-av!+*%#30^7!-t)-@+a@db#*=zgo0*$c(fdo#*#zvds$=^wb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # AUTH_USER_MODEL = 'ristana.CustomUser'
 
